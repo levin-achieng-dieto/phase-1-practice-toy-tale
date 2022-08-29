c7e5b7863c812cebba379dae41df188bee55f0dd
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-08-30 00:08:10 +0300

    assignment

--- src/index.js
+++ src/index.js
@@ -13,3 +13,59 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.addEventListener('DOMContentLoaded', (e) =>{
+  e.preventDefault()
+  getToys()
+  fetchingToys()
+});
+
+function getToys(andys){
+  let toyDiv = document.createElement('div')
+  toyDiv.className = "card"
+  toyDiv.innerHTML = `
+  <h2>${andys.name}</h2>
+  <img class="toy-avatar" src ="${andys.image}">
+  <p>${andys.likes}</p>
+  <button class="like-btn" id="toy_id">${andys.id}</button>`
+
+  document.getElementById("toy-collection").append(toyDiv)
+}
+
+
+function fetchingToys(){
+  fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(toys => toys.forEach(toy => getToys(toy)))
+}
+fetchingToys()
+
+
+function addingToys(addToys){
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body:JSON.stringify(addToys)
+  })
+  .then(response => response.json())
+  .then(toys => console.log(toys))
+}
+
+
+
+
+document.querySelector('.add-toy-form').addEventListener('submit', handleSubmit)
+
+function handleSubmit(e){
+  e.preventDefault()
+  let toyObj = {
+    name:e.target.name.value,
+    image:e.target.image.value,
+    likes:e.target.likes.value,
+    id:e.target.id.value
+  }
+  getToys(toyObj)
+  addingToys(toyObj)
+}
